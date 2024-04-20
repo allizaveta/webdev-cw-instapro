729846c635b20fd038116b2b0023f4d256961316
Allizaveta <allizaveta@gmail.com>
2024-04-20 04:32:17 +0300

    sss

--- components/user-poset-component.js
+++ components/user-poset-component.js
@@ -1,59 +1,68 @@
-
+import { USER_POSTS_PAGE, POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
+import { goToPage } from "../index.js";
 import { escapeHTML } from "../helpers.js";
 
 export function renderUserPageComponent({ appEl, posts }) {
-  const renderPost = (post) => {
-    const postHtml = `
-      <li class="post">
-        <div class="post-header" data-user-id="${post.user.id}">
-          <img src="${post.user.imageUrl}" class="post-header__user-image">
-          <p class="post-header__user-name">${escapeHTML(post.user.name)}</p>
+    // Функция для отображения имени и фото пользователя
+    const renderUserInfo = (user) => {
+      const userInfoHtml = `
+        <div class="user-info">
+          <img src="${user.imageUrl}" class="user-info__user-image">
+          <p class="user-info__user-name">${escapeHTML(user.name)}</p>
         </div>
-        <div class="post-image-container">
-          <img class="post-image" src="${post.imageUrl}">
-        </div>
-        <div class="post-likes">
-          <button data-post-id="${post.id}" class="like-button">
-            <img src="./assets/images/like-active.svg">
-          </button>
-          <p class="post-likes-text">
-            Нравится: <strong>${post.likes}</strong>
+      `;
+      return userInfoHtml;
+    };
+  
+    const renderPost = (post) => {
+      const postHtml = `
+        <li class="post">
+          <div class="post-header" data-user-id="${post.user.id}">
+            <div class="user-info-container"></div> <!-- Здесь будет отображаться имя и фото пользователя -->
+          </div>
+          <div class="post-image-container">
+            <img class="post-image" src="${post.imageUrl}">
+          </div>
+          <div class="post-text">
+            ${escapeHTML(post.description)}
+          </div>
+          <p class="post-date">
+            ${formatDate(post.createdAt)}
           </p>
-        </div>
-        <p class="post-text">
-          <span class="user-name">${escapeHTML(post.user.name)}</span>
-          ${escapeHTML(post.description)}
-        </p>
-        <p class="post-date">
-          ${formatDate(post.createdAt)}
-        </p>
-      </li>
-    `;
-    return postHtml;
-  };
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const now = new Date();
-    const diffInMs = now - date;
-    // Логика форматирования даты, например, "19 минут назад"
-    // Можно использовать библиотеку date-fns для этого
-    // Но для примера я оставлю простое форматирование
-    return `${Math.round(diffInMs / (1000 * 60))} минут назад`;
-  };
-
-  const appHtml = `
-    <div class="page-container">
-      <div class="header-container"></div>
-      <ul class="posts">
-        ${posts.map(renderPost).join('')}
-      </ul>
-    </div>`;
-
-  appEl.innerHTML = appHtml;
-
-  renderHeaderComponent({
-    element: document.querySelector(".header-container"),
-  });
-}
+          <div class="post-likes">
+            <button data-post-id="${post.id}" class="like-button">
+              <img src="./assets/images/like-active.svg">
+            </button>
+            <p class="post-likes-text">
+              Нравится: <strong>${post.likes}</strong>
+            </p>
+          </div>
+        </li>
+      `;
+      return postHtml;
+    };
+  
+    const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      const now = new Date();
+      const diffInMs = now - date;
+      return `${Math.round(diffInMs / (1000 * 60))} минут назад`;
+    };
+  
+    const appHtml = `
+      <div class="page-container">
+        <div class="header-container"></div>
+        ${renderUserInfo(posts[0].user)} <!-- Отображаем имя и фото пользователя -->
+        <ul class="posts">
+          ${posts.map(renderPost).join('')}
+        </ul>
+      </div>`;
+  
+    appEl.innerHTML = appHtml;
+  
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
+  }
+  
